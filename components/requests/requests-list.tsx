0f57b4ebbc828d7810cb7b7265c4891148cf6658
hrithikdas7 <hrithikdas044@gmail.com>
2026-06-17 18:27:28 +0530

    Hydration-err-fixed

--- components/requests/requests-list.tsx
+++ components/requests/requests-list.tsx
@@ -1,6 +1,7 @@
 // components/requests/requests-list.tsx
 'use client'
 
+import { useState, useEffect } from 'react'
 import { ModuleRestrictedState, SelfServiceModuleTabs } from '@/components/common'
 import { useModuleGate } from '@/lib/permissions/use-module-gate'
 import { AdminRequestsList } from './admin-requests-list'
@@ -8,9 +9,14 @@ import { EmployeeRequestsView } from './employee-requests-view'
 import { RequestsSkeleton } from './requests-skeleton'
 
 export function RequestsList() {
+  const [isMounted, setIsMounted] = useState(false)
   const requestsGate = useModuleGate('requests')
 
-  if (requestsGate.isLoading) {
+  useEffect(() => {
+    setIsMounted(true)
+  }, [])
+
+  if (!isMounted || requestsGate.isLoading) {
     return <RequestsSkeleton variant="generic" />
   }
 
